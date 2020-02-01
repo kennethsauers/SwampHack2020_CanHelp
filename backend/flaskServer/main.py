eed14adc5a2ae51796a4db32bf4f44c61c11fceb
kennethsauers <hedonist@LAPTOP-DV63O0BV.localdomain>
2020-02-01 08:21:55 -0500

    added new features to server

---
+++ backend/flaskServer/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+
+modelPath = '/home/kennethsauers/SwampHack2020_CanHelp/backend/savedModels/path_to_my_model.h5'
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST'])
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/calc', methods = ['POST'])
+def calc():
+    content = request.json
+    print(content)
+    return {"data" : content["value"]  * 3}
+
+@app.route('/greeting', methods =['POST'])
+def greeting():
+    content = request.json
+    str = "hello %s of age %d"%(content["name"],content["age"])
+    data = {
+            "greeting" : str,
+            "name" : content["name"],
+            "age": content["age"]
+           }
+    return data
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port = 8080, debug = True)
